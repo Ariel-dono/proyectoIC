e4b03cbdb94d23ac4f1fe694ce1b8c0a007dd417
ariel-dono <herrerafariel.345@gmail.com>
2017-09-03 16:46:02 -0600

    Namespace Modeling

--- Back-End/SiteService/SITE/Model.go
+++ Back-End/SiteService/SITE/Model.go
@@ -1,7 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
+//Routing model
 type Route struct {
 	Name        string
 	Method      string
@@ -11,6 +14,7 @@ type Route struct {
 
 type Routes []Route
 
+//Response messaging
 type Response struct {
 	State      	State    `json:"state"`
 	Code 		int      `json:"code"`
@@ -18,4 +22,31 @@ type Response struct {
 
 type State struct {
 	Message 	string 	 `json:"message"`
+}
+
+//Projects management
+type namespace struct {
+	Projects 	[]string	 `json:"projects"`
+	Owner 		string		 `json:"owner"`
+}
+
+//Project model
+type project struct {
+	Stages		[]vectorSequence `json:"stages"`
+	//Information about project
+	Name 		string 		 `json:"name"`
+	Zoom 		int 		 `json:"zoom"`
+	Reference	Point		 `json:"reference"`
+}
+
+//Functional Areas
+//Nature types: 1. Dynamic Area, 2. Temporal Area, 3. Locked Area, 4.Machinery path 5. workers path
+type vectorSequence struct {
+	VectorsSequence []Point	 `json:"vectors_sequence"`
+	Nature		int 	 `json:"nature"`
+}
+
+type Point struct {
+	X 		int 	`json:"x"`
+	Y		int 	`json:"y"`
 }
